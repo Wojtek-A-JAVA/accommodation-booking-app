ad18011a73fbcad799eae3aa00c0268f06cff83c
Wojtek <wojka66@gmail.com>
2025-12-15 14:53:34 +0100

    Initial commit.

---
+++ src/test/java/accommodation/booking/app/ApplicationTest.java
@@ -0,0 +1,11 @@
+package accommodation.booking.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}
