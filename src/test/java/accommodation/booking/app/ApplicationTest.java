2a044002d42db530ba275c97509612d5b3faf84f
Wojtek <wojka66@gmail.com>
2026-02-19 15:49:00 +0100

    Update ApplicationTest.java

--- src/test/java/accommodation/booking/app/ApplicationTest.java
+++ src/test/java/accommodation/booking/app/ApplicationTest.java
@@ -1,11 +1,10 @@
 package accommodation.booking.app;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class ApplicationTest {
-    @Test
-    void contextLoads() {
-    }
+    // @Test
+    // void contextLoads() {
+    // }
 }
